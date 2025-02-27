568adbfb788ef9647415a5898662a011c719b63c
Matias Gaete <matias.gaete@smithcommerce.com>
2025-02-26 22:29:14 -0300

    replaced static value

--- src/features/shared/components/Snackbar/Snackbar.tsx
+++ src/features/shared/components/Snackbar/Snackbar.tsx
@@ -5,7 +5,7 @@ export const Snackbar = () => {
   const { show, hideSnackbar, message, type } = useSnackbar();
   return (
     <ToastContainer className="p-3" position="top-end" style={{ zIndex: 1000 }}>
-      <Toast bg={type} onClose={hideSnackbar} show={true} delay={3000}>
+      <Toast bg={type} onClose={hideSnackbar} show={show} delay={3000}>
         <Toast.Header closeButton={false}>
           <strong className="me-auto">{type}</strong>
         </Toast.Header>
